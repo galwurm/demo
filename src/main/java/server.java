9e8171a00ed87e62a3e932e7b7af53e507113e96
Gal Wurmbrand <galvurem@gmail.com>
2019-12-29 18:16:16 +0200

    added mongo driver connectios to working db

--- src/main/java/server.java
+++ src/main/java/server.java
@@ -1,3 +1,8 @@
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
 import static spark.Spark.*;
 
 public class server {
@@ -8,6 +13,19 @@ public class server {
             return "Hello: " + request.params(":name");
         });
 
-        
+        initdb();
     }
-}
+
+        private static void initdb() {
+            MongoClientURI uri = new MongoClientURI(
+                    "mongodb+srv://test:Aa123456@cluster0-5w6ij.mongodb.net/test?retryWrites=true&w=majority");
+
+            MongoClient mongoClient = new MongoClient(uri);
+            MongoDatabase database = mongoClient.getDatabase("sample_training");
+
+            MongoCollection coll = database.getCollection("grades");
+
+            System.out.println(coll.countDocuments());
+
+        }
+    }
